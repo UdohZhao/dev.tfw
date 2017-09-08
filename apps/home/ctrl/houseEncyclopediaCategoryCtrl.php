d411c8408c02a9888fa6012017f32602525b4cac
He Jian Qing <17783670639@163.com>
2017-09-08 19:17:35 +0800

    he

--- apps/home/ctrl/houseEncyclopediaCategoryCtrl.php
+++ apps/home/ctrl/houseEncyclopediaCategoryCtrl.php
@@ -3,29 +3,45 @@ namespace apps\home\ctrl;
 use apps\home\model\houseEncyclopediaCategory;
   class houseEncyclopediaCategoryCtrl extends baseCtrl{
   	 public $db;
-  	 public $id;
+  	 public $pid;
+     public $id;
+
   // 构造方法
   public function _auto(){
     $this->db = new houseEncyclopediaCategory();
+    $this->pid = isset($_GET['pid']) ? intval($_GET['pid']) : 0;
     $this->id = isset($_GET['id']) ? intval($_GET['id']) : 0;
   }
   public function sel(){
  
+  	$data = $this->db->sel($this->pid);
+    if ($data) {
+      echo J(R(200,'成功',$data));
+    } else {
+      echo J(R(400,'数据加载异常 :(',false));
+    }
 
-  	$data = $this->db->sel($this->id);
-
-
-  	echo J(R(200,'成功',$data));
   }
 public function seles(){
-  $data = $this->db->seles($this->id);
 
-  echo J(R(200,'成功',$data));
+  $data = $this->db->seles($this->id);
+    if ($data) {
+      echo J(R(200,'成功',$data));
+    } else {
+      echo J(R(400,'数据加载异常 :(',false));
+    }
+
 }
  
  public function sel1(){
  	$data = $this->db->sel1($this->id);
- 	echo J(R(200,'成功',$data));
+ 
+     if ($data) {
+      echo J(R(200,'成功',$data));
+    } else {
+      echo J(R(400,'数据加载异常 :(',false));
+    }
+
   }
 
 
--- apps/home/model/houseEncyclopediaCategory.php
+++ apps/home/model/houseEncyclopediaCategory.php
@@ -5,8 +5,8 @@ class houseEncyclopediaCategory extends model{
 	public $table = 'house_encyclopedia_category';# 购房百科类别表
 	public $table1 = 'house_encyclopedia_article';# 购房百科文章表 
 
-	public function sel($id){
-		$sql = "SELECT * FROM $this->table where status=0 and pid=$id ";
+	public function sel($pid){
+		$sql = "SELECT * FROM $this->table where pid = '$pid' and status='0'";
 
 	    return $this->query($sql)->fetchAll(2);
 	}
