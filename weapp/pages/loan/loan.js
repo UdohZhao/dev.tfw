3ad741f9255f702fcd460ea060569de2b393aa8f
He Jian Qing <17783670639@163.com>
2017-09-09 23:56:31 +0800

    he

--- weapp/pages/loan/loan.js
+++ weapp/pages/loan/loan.js
@@ -113,7 +113,7 @@ Page({
               success: function (res) {
                 if (res.confirm) {
                   console.log('用户点击确定')
-                  wx.reLaunch({
+                  wx.redirectTo({
                     url: '/pages/loan/loan'
                   })
                 }
